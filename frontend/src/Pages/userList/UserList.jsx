5852cc04a5442d2256a798e9a5cbc20b74d32d9e
TaariqDeveloper <TaariqDeveloper@gmail.com>
2025-03-20 11:22:14 +0300

    update

--- frontend/src/Pages/userList/UserList.jsx
+++ frontend/src/Pages/userList/UserList.jsx
@@ -631,7 +631,7 @@ function UserList() {
                   onClick={handleUpdate}
                   className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 transition"
                 >
-                  Save
+                  Savee
                 </button>
               </div>
             </motion.div>
