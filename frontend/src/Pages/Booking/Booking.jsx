9b8b1d075aeeff44add71c342570bd3b0a5bde5a
SabirinMohamudAdan <sabrinadan327@gmail.com>
2025-03-21 14:01:26 -0700

    new update

--- frontend/src/Pages/Booking/Booking.jsx
+++ frontend/src/Pages/Booking/Booking.jsx
@@ -1138,7 +1138,7 @@ const Booking = () => {
           initial={{ opacity: 0, y: -50 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8 }}
-          className="  bg-gradient-to-r from-white shadow-2xl rounded-2xl p-8 w-full
+          className="  bg-gradient-to-r from-white shadow-4xl rounded-2xl p-8 w-full
            max-w-4xl flex flex-col md:flex-row z-10 "
         >
           {/* Form Section */}
